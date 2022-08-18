5b21f7070fcdcc0585acb9db58698b6321e23962
marina <litvinova_06@bk.ru>
2022-08-18 21:32:27 +0500

    work1

--- work_1/Program.cs
+++ work_1/Program.cs
@@ -1,30 +1,4 @@
-﻿// class Program
-// {
-//     public static void Main (string[]args)
-//     {
-//         Console.Write("Введите число которое будем возводить в степень: ");
-
-//         int number = Convert.ToInt32(Console.ReadLine());
-
-//         if (number <1) Console.WriteLine (" ");
-
-//         Console.WriteLine($"Ваше число {number}");
-
-//         Console.Write ("Введите число степени:");
-
-//         int N = Convert.ToInt32(Console.ReadLine());
-
-//         if (N <1) Console.WriteLine (" ");
-
-//         for ( int i =1; i< number; i++)
-//         {
-//             Console.WriteLine (MathF.Pow (i+1, N) +";");
-//         }
-
-//     }
-// }
-
-
+﻿
 class Program
 {
     public static void Main (string[]args)
