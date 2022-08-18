a06ebd1d79a3b28f192d371976a4c6e998d75b2e
marina <litvinova_06@bk.ru>
2022-08-18 23:35:33 +0500

    work3

---
+++ work_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    static int[] CreaterArray (int number)
+    {
+        int[]array =new int [number];
+        for (int i=0; i< array.Length; i++)
+        {
+            array[i] = i +1;
+        } 
+        return array;
+
+    }
+
+    static void WriteArray(int[]array)
+    {
+        Console.WriteLine("Вывод массива");
+        for (int i=0; i< array.Length; i++)
+        {
+            Console.Write(array [i] + " ");
+        }
+    }
+    static void Main (string[]args)
+    {
+        Console.Write("Введите колличество чисел в массиве: ");
+
+        int N = Convert.ToInt32(Console.ReadLine());
+        int[]array = CreaterArray(N);
+        WriteArray (array);
+        Console.ReadLine();
+    }
+}
